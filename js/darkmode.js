1ef4a3936840bedaa27b0169b8a2a836903bbe61
Cássio Venâncio de Souza <cassiovenanciosouza@gmail.com>
2025-09-06 16:21:11 -0300

    DarkModeFinalizado

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,1 +1,85 @@
-var typeLight = "day";
+window.currentTypeLight = 'Day';
+
+let menuSideBarColorNight = '#1C1C1C';
+let navBarTabsColorNight = menuSideBarColorNight;
+let backgroundSiteColorNight = ['rgb(32, 32, 32)', 'rgb(43, 43, 43)'];
+
+let menuSideBarColorDay = '#EBEBEB';
+let navBarTabsColorDay = menuSideBarColorDay;
+let backgroundSiteColorDay = ['#EAEAEA', '#CECECE'];
+
+let menuSideBar = document.getElementById('Container');
+let navBar = document.getElementById('NavBar');
+let backgroundPage = document.getElementById('PageContent');
+let switchLight = document.getElementById('SwitchLight');
+
+let imgLight = document.getElementById('Day');
+let imgNight = document.getElementById('Night');
+
+
+function ChangeLight(light)
+{
+    GetAllCurrentOpenTabsAndChangeBackground();
+    RemoveCurrentLightActiveMenu();
+    window.currentTypeLight = light;
+    AddCurrentLightActiveMenu();
+    ChangeLightImage();
+}
+
+function ChangeLightImage(){
+
+    if(window.currentTypeLight == 'Day'){
+        imgLight.style.display = 'none';
+        imgNight.style.display = 'block';
+        ChangeColorToDay();
+    }else{
+        imgLight.style.display = 'block';
+        imgNight.style.display = 'none';
+        ChangeColorToNight();
+    }
+}
+
+function GetAllCurrentOpenTabsAndChangeBackground(){
+    let tabs = [];
+    if(window.currentTypeLight == 'Day'){
+        tabs = document.querySelectorAll('.Page__Content__NavBar__OptionDay');
+        tabs.forEach(tab =>{
+            tab.classList.add('Page__Content__NavBar__OptionNight');
+            tab.classList.remove('Page__Content__NavBar__OptionDay');
+        });
+    }else{
+        tabs = document.querySelectorAll('.Page__Content__NavBar__OptionNight')
+        tabs.forEach(tab =>{
+            tab.classList.remove('Page__Content__NavBar__OptionNight');
+            tab.classList.add('Page__Content__NavBar__OptionDay');
+        });
+    }
+}
+
+function RemoveCurrentLightActiveMenu(){
+    let activeMenu = document.querySelector(`.ActiveMenu${window.currentTypeLight}`);
+    activeMenu.classList.remove(`ActiveMenu${window.currentTypeLight}`);
+    activeMenu.classList.add('ActiveMenu');
+}
+
+function AddCurrentLightActiveMenu(){
+    let activeMenu = document.querySelector(`.ActiveMenu`);
+    activeMenu.classList.remove('ActiveMenu');
+    activeMenu.classList.add(`ActiveMenu${window.currentTypeLight}`);
+}
+
+function ChangeColorToDay(){
+    menuSideBar.style.backgroundColor = menuSideBarColorDay;
+    menuSideBar.style.color = '#1C1C1C';
+    navBar.style.backgroundColor = navBarTabsColorDay;
+    backgroundPage.style.backgroundImage = `linear-gradient(to right, ${backgroundSiteColorDay[0]}, ${backgroundSiteColorDay[1]})`
+    switchLight.style.backgroundColor = navBarTabsColorDay;
+}
+
+function ChangeColorToNight(){
+    menuSideBar.style.backgroundColor = menuSideBarColorNight;
+    menuSideBar.style.color = '#f3f1f1ff';
+    navBar.style.backgroundColor = navBarTabsColorNight;
+    backgroundPage.style.backgroundImage = `linear-gradient(to right, ${backgroundSiteColorNight[0]}, ${backgroundSiteColorNight[1]})`
+    switchLight.style.backgroundColor = navBarTabsColorNight;
+}
--- js/general.js
+++ js/general.js
@@ -1,3 +1,4 @@
+
 let pagesList = ['.AboutPage', '.QualificationsPage', '.StacksPage', '.ExperiencePage', '.PlataformPage'];
 let menusList = ['.AboutMenu', '.QualificationsMenu', '.StacksMenu', '.ExperienceMenu', '.PlataformMenu'];
 let tabs = ['.About'];
@@ -9,9 +10,11 @@ let deactiveColor = '#1C1C1C';
 function CreateTab(nameClass) 
 {
     const documentHasTab = document.getElementsByClassName(nameClass);
+    let classCurrentLight = `Page__Content__NavBar__Option${window.currentTypeLight}`;
+    console.log(classCurrentLight);
 
     if(documentHasTab.length < 1){
-        document.getElementById("NavBar").innerHTML += `<div class="Page__Content__NavBar__Option ${nameClass}"><div class="Page__Content__NavBar__Option__IconAndText" onclick="ShowInfos('${nameClass}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-html" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z"/></svg><span>${nameClass}</span></div><div class="Page__Content__NavBar__Option__XExit"><span class="exit" onclick="DeleteTab('${nameClass}')">x</span></div></div>`;
+        document.getElementById("NavBar").innerHTML += `<div class="${classCurrentLight} ${nameClass}"><div class="Page__Content__NavBar__Option__IconAndText" onclick="ShowInfos('${nameClass}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-html" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z"/></svg><span>${nameClass}</span></div><div class="Page__Content__NavBar__Option__XExit"><span class="exit" onclick="DeleteTab('${nameClass}')">x</span></div></div>`;
         tabs.push('.' + nameClass);
         activeTab = '.' + nameClass;
         ActiveMenu(nameClass);
@@ -39,7 +42,7 @@ function DeleteTab(nameClass){
 function ActiveMenu(nameClass){
     let classMenu = '.' + nameClass + 'Menu';
     DeactivateAllMenus();
-    document.querySelector(classMenu).classList.add('ActiveMenu');
+    document.querySelector(classMenu).classList.add(`ActiveMenu${window.currentTypeLight}`);
     return;
 }
 
@@ -53,7 +56,7 @@ function HideAllPages()
 
 function DeactivateAllMenus(){
     menusList.forEach(element => {
-        document.querySelector(element).classList.remove('ActiveMenu');
+        document.querySelector(element).classList.remove(`ActiveMenu${window.currentTypeLight}`);
     });
     return;
 }
